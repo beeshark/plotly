f66b753ef454c9183075f175ce0eac8e1227b4a6
Sara <sbehnke33@yahoo.com>
2020-11-17 17:38:34 -0600

    pseudocode

---
+++ app.js
@@ -0,0 +1,16 @@
+// retrive data with d3
+
+
+// horizontal bar chart with dropdown menufor top 10 OTUs. otu_ids = barchart labels / otu labels hovertext
+
+
+// bubble chart. otu_ids = x AND marker colors / sample_values = y AND marker size / otu_labels = text values
+
+
+// display cample metadata ex) indv demographic info
+
+
+// display each key value pair from the metadata json object somewhere
+
+
+// update all of the plots any time that a new sample is selected
