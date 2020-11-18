cad362f10caeab81d6fa4a677ac5b6c6f7913d71
Sara <sbehnke33@yahoo.com>
2020-11-17 18:10:44 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 
 
 // retrive data with d3
-const url = "data/samples.json"
-d3.json(url).then(function(data) {
+var samples = "data/samples.json"
+
+function buildMetadata(sample){
+    console.log(sample)
+d3.json(path).then(function(data) {
     console.log(data);
   });
+
+
+ function buildPlots(id)
 
 // horizontal bar chart with dropdown menu for top 10 OTUs. otu_ids = barchart labels / otu labels hovertext
 var horz = {
@@ -22,13 +28,13 @@ var dataset = dropdownMenu.property("value");
 
 // bubble chart. otu_ids = x AND marker colors / sample_values = y AND marker size / otu_labels = text values
 var bubble = {
-    x: samples.otu_ids,
-    y: samples.sample_values,
+    x: sample.otu_ids,
+    y: sample.sample_values,
     mode: "markers",
     text: sample.otu_labels,
     marker:{
-        size: samples.sample_values,
-        color: samples.otu_ids
+        size: sample.sample_values,
+        color: sample.otu_ids
     }
 
 }
@@ -40,3 +46,5 @@ var bubble = {
 
 
 // update all of the plots any time that a new sample is selected
+
+}
